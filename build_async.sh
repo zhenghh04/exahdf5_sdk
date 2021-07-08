df43ef0fedae73298bc1cefbe3ece18d5a57b3e7
Huihuo Zheng <zhenghh04@gmail.com>
2021-07-08 14:37:06 -0500

    fixed build issue for Async VOL on Mac systems

--- build_async.sh
+++ build_async.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git clone https://github.com/hpc-io/vol-async
 cd vol-async/src
-sed -e "s/HDF5_DIR =/#HDF5_DIR =/g" -e "s/ABT_DIR =/#ABT_DIR =/g" -e "s/CC = cc/CC=mpicc/g" Makefile > Makefile.local
+sed -e "s/HDF5_DIR =/#HDF5_DIR =/g" -e "s/ABT_DIR =/#ABT_DIR =/g" -e "s/CC = cc/CC=mpicc/g" -e "s/\$(STATHDR)//g" Makefile > Makefile.local
 make -f Makefile.local
 cp -v lib* $HDF5_VOL_DIR/lib
 cp -v *.h $HDF5_VOL_DIR/include
