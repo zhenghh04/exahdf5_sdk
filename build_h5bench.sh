05c212c67fc5e4cb9bce567c2dba6ea5033c426b
Huihuo Zheng <zhenghh04@gmail.com>
2021-08-26 16:30:07 -0500

    added cache + async vols for h5bench

--- build_h5bench.sh
+++ build_h5bench.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/hpc-io/h5bench.git
 mkdir h5bench/build
 cd h5bench/build
-cmake .. -DCMAKE_INSTALL_PREFIX=$SDK_DIR/h5bench
-make all install
+cmake .. -DCMAKE_INSTALL_PREFIX=$SDK_DIR/h5bench  -DWITH_CACHE_VOL:BOOL=ON -DWITH_ASYNC_VOL:BOOL=ON -DCMAKE_C_FLAGS="-I/$HDF5_VOL_DIR/include -L/$HDF5_VOL_DIR/lib"
+
+make all install V=1
 cd -
