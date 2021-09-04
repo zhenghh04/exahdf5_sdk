c4a6e79ffcfe032e349c288e1f644d2f964fac83
Huihuo Zheng <zhenghh04@gmail.com>
2021-09-04 00:47:24 -0500

    defined compiler in h5bench

--- build_h5bench.sh
+++ build_h5bench.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/hpc-io/h5bench.git
 mkdir h5bench/build
 cd h5bench/build
-cmake .. -DCMAKE_INSTALL_PREFIX=$SDK_DIR/h5bench  -DWITH_CACHE_VOL:BOOL=ON -DWITH_ASYNC_VOL:BOOL=ON -DCMAKE_C_FLAGS="-I/$HDF5_VOL_DIR/include -L/$HDF5_VOL_DIR/lib -g"
+cmake .. -DCMAKE_INSTALL_PREFIX=$SDK_DIR/h5bench  -DWITH_CACHE_VOL:BOOL=ON -DWITH_ASYNC_VOL:BOOL=ON -DCMAKE_C_FLAGS="-I/$HDF5_VOL_DIR/include -L/$HDF5_VOL_DIR/lib -g" -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx
 
 make all install VERBOSE=1
 cd -
