4e548c9eca7ac921fb0d0ddd42b31803991c33f1
Huihuo Zheng <huihuo.zheng@anl.gov>
2022-06-03 16:46:55 -0500

    added other tests

--- build_vol_tests.sh
+++ build_vol_tests.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/HDFGroup/vol-tests.git
 cd vol-tests
 [ -e build ] || mkdir build
-CC=mpicc CXX=mpicxx HDF5_DIR=$HDF5_ROOT cmake ..
+cd build
+HDF5_DIR=$HDF5_ROOT cmake .. -DHDF5_DIR=$HDF5_ROOT -DHDF5_VOL_TEST_ENABLE_PARALLEL=ON -DH\
+DF5_VOL_TEST_ENABLE_ASYNC=ON -DHDF5_VOL_TEST_ENABLE_PART=ON
 make
